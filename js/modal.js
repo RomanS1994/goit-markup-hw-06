9bf2f75753f81b659f4db9aadbdd9079151ac458
Roman Stryzhka <rstrizhko1994@gmail.com>
2025-03-16 19:26:58 +0100

    add modal

---
+++ js/modal.js
@@ -0,0 +1,12 @@
+const modal = document.querySelector('[data-modal]');
+const modalOpenBtn = document.querySelector('[data-modal-open]');
+const modalCloseBtn = document.querySelector('[data-modal-close]');
+
+modalOpenBtn.addEventListener('click', toggleModal);
+modalCloseBtn.addEventListener('click', toggleModal);
+
+function toggleModal() {
+  modal.classList.toggle('is-open');
+}
+
+console.log(document.forms);
